938f8abde0ef762ef3a6b85ff0e6ad199ef30b90
johngiorgi <johnmgiorgi@gmail.com>
2021-02-25 17:56:34 -0500

    :white_check_mark: Add tests for Seq2Rel

--- tests/test_seq2rel.py
+++ tests/test_seq2rel.py
@@ -3,3 +3,10 @@ from seq2rel import __version__
 
 def test_version():
     assert __version__ == "0.1.0"
+
+
+class TestSeq2Rel:
+    def test_ade_model(self, pretrained_ade_model, ade_examples):
+        texts, expected = ade_examples
+        actual = pretrained_ade_model(texts)
+        assert actual == expected

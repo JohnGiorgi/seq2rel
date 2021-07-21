32e57cca9c0aaaaa85f2bb5d0123b1ec3827e2c1
johngiorgi <johnmgiorgi@gmail.com>
2021-07-21 17:23:08 -0400

    :heavy_plus_sign: Add wasabi printer

--- seq2rel/__init__.py
+++ seq2rel/__init__.py
@@ -1,4 +1,7 @@
-__version__ = "0.1.0"
+from wasabi import Printer
 
-from seq2rel.seq2rel import Seq2Rel
-from seq2rel.seq2rel import PRETRAINED_MODELS
+from seq2rel.seq2rel import PRETRAINED_MODELS, Seq2Rel
+
+msg = Printer(line_max=100)
+
+__version__ = "0.1.0"

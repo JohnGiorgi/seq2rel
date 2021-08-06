042aab3e63e21c6112ff8f9748214b57c9fc11f1
johngiorgi <johnmgiorgi@gmail.com>
2021-08-06 14:37:18 -0400

    :white_check_mark: Add explicit check for ordered_ents=True

--- tests/metrics/test_fbeta_measure_seq2rel.py
+++ tests/metrics/test_fbeta_measure_seq2rel.py
@@ -226,6 +226,37 @@ class TestFBetaMeasureSeq2Rel(FBetaMeasureSeq2RelTestCase):
         assert isinstance(recalls, List)
         assert isinstance(fscores, List)
 
+    def test_fbeta_seq2rel_multiclass_metric_ordered_ents(self):
+        fbeta = FBetaMeasureSeq2Rel(labels=self.labels, ordered_ents=True)
+        fbeta(self.predictions, self.targets)
+        metric = fbeta.get_metric()
+        precisions = metric["precision"]
+        recalls = metric["recall"]
+        fscores = metric["fscore"]
+
+        # With `ordered_ents=True`, one of the predictions for the class at index 0 is now incorrect.
+        # decrement the true positives by 1 and recompute the desired values.
+        true_positive_sum = copy.deepcopy(self.true_positive_sum)
+        desired_precisions = copy.deepcopy(self.desired_precisions)
+        desired_recalls = copy.deepcopy(self.desired_recalls)
+        true_positive_sum[0] -= 1
+        desired_precisions[0] = (true_positive_sum[0]) / self.pred_sum[0]
+        desired_recalls[0] = (true_positive_sum[0]) / self.true_sum[0]
+        desired_fscores = [
+            (2 * p * r) / (p + r) if p + r != 0.0 else 0.0
+            for p, r in zip(desired_precisions, desired_recalls)
+        ]
+
+        # check value
+        assert_allclose(precisions, desired_precisions)
+        assert_allclose(recalls, desired_recalls)
+        assert_allclose(fscores, desired_fscores)
+
+        # check type
+        assert isinstance(precisions, List)
+        assert isinstance(recalls, List)
+        assert isinstance(fscores, List)
+
     def test_fbeta_seq2rel_multiclass_macro_average_metric(self):
         fbeta = FBetaMeasureSeq2Rel(labels=self.labels, average="macro")
         fbeta(self.predictions, self.targets)

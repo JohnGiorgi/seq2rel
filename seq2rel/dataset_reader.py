28c4e6a7eed40d46c7d86b7e2e6991f0273cdd47
John Giorgi <johnmgiorgi@gmail.com>
2022-01-18 09:39:17 -0500

    Head+tail truncation (#215)
    * :sparkles: Add max_length argument and support head+tail truncation
    
    * :white_check_mark: Update tests

--- seq2rel/dataset_reader.py
+++ seq2rel/dataset_reader.py
@@ -1,9 +1,12 @@
 import logging
+from math import floor
+from typing import Optional
 
 from allennlp.data.dataset_readers.dataset_reader import DatasetReader
 from allennlp.data.instance import Instance
 from allennlp_models.generation.dataset_readers import CopyNetDatasetReader
 from overrides import overrides
+from allennlp.data.tokenizers import PretrainedTransformerTokenizer
 
 logger = logging.getLogger(__name__)
 
@@ -12,15 +15,34 @@ logger = logging.getLogger(__name__)
 class Seq2RelDatasetReader(CopyNetDatasetReader):
     """
     This is a thin wrapper around `CopyNetDatasetReader` to provide any of the modifications
-    necessary to use the dataset reader for information extraction. The arguments are identical to
-    `CopyNetSeq2Seq`. For details, please see:
-    [`CopyNetSeq2Seq`](https://github.com/allenai/allennlp-models/blob/main/allennlp_models/generation/dataset_readers/copynet_seq2seq.py),
+    necessary to use the dataset reader for information extraction. For details, please see:
+    [`CopyNetSeq2Seq`](https://github.com/allenai/allennlp-models/blob/main/allennlp_models/generation/dataset_readers/copynet_seq2seq.py).
+
+    # Parameters
+
+    max_length : `int`, optional (default = None)
+        The maximum length of the source sequences. If not None, the source sequences will be
+        truncated to this length by concatenating the first 75% and last 25% `max_length` tokens.
+        This is known as head+tail truncation. See https://arxiv.org/abs/1905.05583 for details.
+        Note: This only works correctly if the source tokenizer is a `PretrainedTransformerTokenizer`.
     """
+
+    def __init__(self, max_length: Optional[int] = None, **kwargs) -> None:
+        super().__init__(**kwargs)
+        self._max_length = max_length
 
     @overrides
     def text_to_instance(
         self, source_string: str, target_string: str = None, _id: str = None
     ) -> Instance:  # type: ignore
+
+        if self._max_length is not None:
+            if not isinstance(self._source_tokenizer, PretrainedTransformerTokenizer):
+                raise ValueError(
+                    "max_length was provided to Seq2RelDatasetReader. source_tokenizer must be a"
+                    " PretrainedTransformerTokenizer."
+                )
+            source_string = self._head_tail_truncation(source_string)
         # We have to add a space in front of the source/target strings in order to achieve
         # consistant tokenization with certain tokenizers, like GPT. Enforce this behavior here.
         # See: https://github.com/huggingface/transformers/issues/1196
@@ -32,3 +54,24 @@ class Seq2RelDatasetReader(CopyNetDatasetReader):
         if _id is not None:
             instance.fields["metadata"].metadata["_id"] = _id
         return instance
+
+    def _head_tail_truncation(self, source_string: str) -> str:
+        """Truncates and returns `source_string` by concatenating the first 75% and last 25% of
+        `self._max_length` tokens. Accounts for special tokens added by the tokenizer. If
+        `source_string` is less than `self._max_length`, it is returned unmodified.
+        """
+        # Account for special tokens
+        max_length = self._max_length - self._source_tokenizer.num_special_tokens_for_sequence()
+        tokenized_source = self._source_tokenizer.tokenizer.tokenize(source_string)
+        if len(tokenized_source) > max_length:
+            # Truncate by concatenating the first 75% of tokens and the last 25% of tokens
+            head = floor(max_length * 0.75)
+            tail = floor(max_length * 0.25)
+            tail += max_length - head - tail
+            tokenized_source = (
+                tokenized_source[:head] + tokenized_source[len(tokenized_source) - tail :]
+            )
+            source_string = self._source_tokenizer.tokenizer.convert_tokens_to_string(
+                tokenized_source
+            )
+        return source_string

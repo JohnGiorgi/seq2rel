52a3e1b42920dfd3e2bf7aa320f5f049e1b878fc
johngiorgi <johnmgiorgi@gmail.com>
2022-01-25 16:54:46 -0500

    :recycle: Handle empty sequences in average length

--- seq2rel/metrics/average_length.py
+++ seq2rel/metrics/average_length.py
@@ -12,29 +12,39 @@ class AverageLength(Metric):
     supports_distributed = False
 
     def __init__(self):
-        self._decoded_lengths = []
+        self._prediction_lengths = []
         self._target_lengths = []
 
     def __call__(self, predictions: List[List[str]], targets: List[List[str]]) -> None:
         for pred, target in zip(predictions, targets):
-            self._decoded_lengths.append(len(pred))
+            self._prediction_lengths.append(len(pred))
             self._target_lengths.append(len(target))
 
     def get_metric(self, reset: bool = False) -> Dict[str, float]:
-        decoded_mean_length = sum(self._decoded_lengths) / len(self._decoded_lengths)
-        target_mean_length = sum(self._target_lengths) / len(self._target_lengths)
+        prediction_mean_length = (
+            sum(self._prediction_lengths) / len(self._prediction_lengths)
+            if len(self._prediction_lengths) > 0
+            else 0
+        )
+        target_mean_length = (
+            sum(self._target_lengths) / len(self._target_lengths)
+            if len(self._target_lengths) > 0
+            else 0
+        )
 
         if reset:
             self.reset()
 
         return {
-            "predictions_mean_length": round(decoded_mean_length, 2),
+            "predictions_mean_length": round(prediction_mean_length, 2),
             "targets_mean_length": round(target_mean_length, 2),
             "predictions_to_targets_length_ratio": round(
-                decoded_mean_length / target_mean_length, 2
-            ),
+                prediction_mean_length / target_mean_length, 2
+            )
+            if target_mean_length > 0
+            else 0,
         }
 
     def reset(self) -> None:
-        self._decoded_lengths = []
+        self._prediction_lengths = []
         self._target_lengths = []
